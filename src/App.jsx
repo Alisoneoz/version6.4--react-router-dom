783ed35194c636787474e669c3a073f4b577ff2b
Alison Estephany <alisonestephanyoz@gmail.com>
2023-03-04 21:36:38 -0400

    Add comments with the step by step of the process of creating a basic navigation system

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+//STEP 1: import these elements from react-router-dom
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -12,11 +13,13 @@ import Page1 from "./pages/Page1"
 import Page2 from "./pages/Page2"
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Root />}>
-        <Route index element={<Home />} />
-        <Route path="/page1" element={<Page1 />} />
+  const router = createBrowserRouter( //STEP 2: create a variable named router and pass createBrowserRouter()
+    createRoutesFromElements( //STEP 3: Pass createRoutesFromElements()
+      <Route path="/" element={<Root />}> {/*//STEP 4: Add a <Route> tag as parent tag and pass path --> / and element <Root /> to it */}
+
+      {/*STEP 6: define your actual routes*/}
+        <Route index element={<Home />} /> {/*STEP 6.1: home page or initial URL -> pass index instead of path, add the element prop as usual*/}
+        <Route path="/page1" element={<Page1 />} /> {/*STEP 6.2: for the other pages add the path and element props, as usual*/}
         <Route path="/page2" element={<Page2 />} />
       </Route>
     )
@@ -24,19 +27,20 @@ function App() {
 
   return (
     <div className="App ">
-      <RouterProvider router={router}/>
+      <RouterProvider router={router}/> {/* STEP 7: Add a <RouterProvider> tag and pass the router as prop, to the App component's JSX */}
+
     </div>
   );
 }
 export default App;
 
-const Root = () => {
+const Root = () => { //STEP 5 create the Root component
   return (
-    <>
-      <div>
+    <> {/* STEP 5.1: Add a fragment as parent tag*/}
+      <div> {/* STEP 5.2: in a div pass to it the components of the navbar (or the navbar component) */}
         <Navbar />
       </div>
-      <div>
+      <div> {/* STEP 5.3:  add anoter div and pass the <Outlet /> component from react-router-dom*/}
         <Outlet />
       </div>
     </>
