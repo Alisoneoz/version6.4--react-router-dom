28a1924bdad38f733c1a36adbceaa6136a7059b7
Alison Estephany <alisonestephanyoz@gmail.com>
2023-03-04 21:24:21 -0400

    Add router functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home"
+import Page1 from "./pages/Page1"
+import Page2 from "./pages/Page2"
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  )
-}
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Route>
+    )
+  );
 
-export default App
+  return (
+    <div className="App ">
+      <RouterProvider router={router}/>
+    </div>
+  );
+}
+export default App;
+
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
